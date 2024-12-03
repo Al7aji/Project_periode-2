9b52c75cc66b9723e24dcc31c731d9347d1590f0
Al7aji <hseen.al.haji05@gmail.com>
2024-12-03 12:17:58 +0100

    cookiemessage en dark mode1

---
+++ main.js
@@ -0,0 +1,10 @@
+document.getElementById("accept-cookie").addEventListener("click",function (){
+    
+   document.getElementById("cookie-message").style.display = "none";
+     
+} );
+
+function lichtmoed(){
+document.getElementById("body").classList.toggle("dark-mode")
+
+}
