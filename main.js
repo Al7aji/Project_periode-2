d616c61924e34a0a4fa3aa3b93035a86de6a5126
Al7aji <hseen.al.haji05@gmail.com>
2024-12-03 12:20:19 +0100

    .

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ document.getElementById("accept-cookie").addEventListener("click",function (){
     
    document.getElementById("cookie-message").style.display = "none";
      
-});
+} );
 
+function lichtmoed(){
+document.getElementById("body").classList.toggle("dark-mode")
 
-  
-
- 
+}
